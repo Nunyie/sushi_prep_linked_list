d955917667852fb172cc5005a353a796f9930f26
Nunyie <roswellbunag05@gmail.com>
2025-10-19 18:07:42 +0800

    Created SushiNode and LinkedList and defined insert at beggining and end

--- sushi_preparation.py
+++ sushi_preparation.py
@@ -1,3 +1,27 @@
 # Nunyie
 # BSCpE 2-2
 # Sushi Preparation with Linked List
+
+class SushiNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beggining(self, data):
+        new_node = SushiNode(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = SushiNode(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
